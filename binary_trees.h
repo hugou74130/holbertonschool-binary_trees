94a7750fc6fc8a927fc314dab7674ec66f0103d0
Hugo <hugoramos1466@gmail.com>
2025-12-08 14:24:23 +0100

    task 1 finish

--- binary_trees.h
+++ binary_trees.h
@@ -26,6 +26,8 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
 /* Déclarations de fonctions (à ajouter selon les tâches) */
 
 #endif /* BINARY_TREES_H */
