8aa3ba5a54b8b5fbc4425ebe9105f2991d24f626
DaRKkem <damross446@gmail.com>
2025-12-09 17:42:38 +0100

    end_13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -4,7 +4,8 @@
  * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree
  * @tree: Pointer to the root node of the tree to measure
  *
- * Return: Number of nodes with at least 1 child in the tree, or 0 if tree is NULL
+ * Return: Number of nodes with at least 1 child in the tree, 
+ * or 0 if tree is NULL
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
