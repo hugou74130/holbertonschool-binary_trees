5af01900c975cbea75de4e3dc82946c94bc9038e
DaRKkem <damross446@gmail.com>
2025-12-09 17:24:22 +0100

    end_11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -10,20 +10,8 @@
 size_t binary_tree_size(const binary_tree_t *tree)
 
 {
-	size_t left_height;
-	size_t right_height;
-
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	if (left_height > right_height)
-		return (left_height + 1);
-	else
-		return (right_height + 1);
+	return (1 + binary_tree_height(tree->left) + binary_tree_height(tree->right));
 }
