f7bde05b34a87d3e887c4b2c2409b427f0f9b265
DaRKkem <damross446@gmail.com>
2025-12-09 14:53:05 +0100

    header_changed

--- binary_trees.h
+++ binary_trees.h
@@ -30,6 +30,19 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 
+void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *tree);
+
+/* Déclarations de fonctions (à ajouter selon les tâches) */
+
+/*
+
 void binary_tree_print(const binary_tree_t *tree)
 {
     int h = 0, i, j;
@@ -68,15 +81,6 @@ void binary_tree_print(const binary_tree_t *tree)
     }
 }
 
-void binary_tree_delete(binary_tree_t *tree);
-int binary_tree_is_leaf(const binary_tree_t *node);
-int binary_tree_is_root(const binary_tree_t *node);
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
-size_t binary_tree_height(const binary_tree_t *tree);
-size_t binary_tree_depth(const binary_tree_t *tree);
-
-/* Déclarations de fonctions (à ajouter selon les tâches) */
+*/
 
 #endif /* BINARY_TREES_H */
