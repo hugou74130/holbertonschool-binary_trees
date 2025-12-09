0ed2ae7aaf7b7006251bca3598915c7d6b8cec27
DaRKkem <damross446@gmail.com>
2025-12-09 18:25:12 +0100

    end_14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,18 +8,16 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree)
 		return 0;
 
-	if (tree->left || tree->right)
-	{
-		if (tree->left && tree->right)
-			return (1 + binary_tree_balance(tree->left) - (1 + binary_tree_balance(tree->right)));
-		if (tree->left)
-			return (1 + binary_tree_balance(tree->left));
-		if (tree->right)
-			return (-1 - binary_tree_balance(tree->right));
-	}
+	int left_height = 0;
+	int right_height = 0;
 
-	return (0);
+	if (tree->left)
+		left_height = 1 + binary_tree_balance(tree->left);
+	if (tree->right)
+		right_height = 1 + binary_tree_balance(tree->right);
+
+	return (left_height - right_height);
 }
