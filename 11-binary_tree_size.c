ce77432c9053890fa733f70d35defae78fd35324
Hugo <hugoramos1466@gmail.com>
2025-12-09 15:56:00 +0100

    11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,8 +7,12 @@
  * Return: Height of the tree, or 0 if tree is NULL
  */
 
-size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree)
+
 {
+	size_t left_height;
+	size_t right_height;
+
 	if (tree == NULL)
 		return (0);
 
