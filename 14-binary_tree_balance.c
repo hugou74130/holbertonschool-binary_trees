17576d561b75750bb35bbebbfee921db75e7409a
Hugo <hugoramos1466@gmail.com>
2025-12-09 18:46:42 +0100

    add binary tree height

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,7 +6,7 @@
  *
  * Return: Height of the tree, or 0 if tree is NULL
  */
-int binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	int left_height;
 	int right_height;
