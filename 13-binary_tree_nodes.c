e8a9146620a6a293c7cf05446e8621ddf3e3b3d8
DaRKkem <damross446@gmail.com>
2025-12-09 17:39:15 +0100

    end_13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -14,5 +14,5 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree->left || tree->right)
 		return (1);
 	else
-		return (binary_tree_bodes(tree->left) + binary_tree_nodes(tree->right));
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
