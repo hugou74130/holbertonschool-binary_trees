680fa26f0eadcadaac3f4f83e4b0c5e1ba56ec1f
DaRKkem <damross446@gmail.com>
2025-12-09 17:25:01 +0100

    end_11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -13,5 +13,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return (1 + binary_tree_height(tree->left) + binary_tree_height(tree->right));
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
