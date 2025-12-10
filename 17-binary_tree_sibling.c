0d22afaade4dd9f0120e0a38d0e608d7930eeaec
DaRKkem <damross446@gmail.com>
2025-12-10 17:58:42 +0100

    end_17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,13 +11,13 @@
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
-		return NULL;
+		return (NULL);
 
 	if (node->parent->left && node->parent->left != node)
-		return node->parent->left;
+		return (node->parent->left);
 
 	if (node->parent->right && node->parent->right != node)
-		return node->parent->right;
+		return (node->parent->right);
 
-	return NULL;
+	return (NULL);
 }
